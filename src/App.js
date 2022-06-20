80bd90310d7e775e52c67844a9f7c7791baea6ce
pimurphs <pi.rodrigues@live.com>
2022-06-20 11:17:40 +0100

    Added Git repo URL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/filipadaguiarrodrigues/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Filipa Rodrigues
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/pimurphs/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
