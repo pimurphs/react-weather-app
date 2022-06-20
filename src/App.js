a7e066a8b49620edec6255ac69faf85aa7d19c7b
pimurphs <pi.rodrigues@live.com>
2022-06-20 11:32:42 +0100

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/filipadaguiarrodrigues/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Filipa Rodrigues
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/pimurphs/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/filipadaguiarrodrigues/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Filipa Rodrigues
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/pimurphs/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
